410fc13baa102559c8bf53dcac98d2ad6d0259e4
Unaisa Jumaila <unaisajumaila@gmail.com>
2026-03-14 12:09:06 +0530

    Part 2: Added shopping cart functionality

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,17 @@
 import "../styles/Header.css";
 
-function Header() {
+function Header({ cartItemCount, onCartClick }) {
   return (
     <header className="header">
       <h1>QuickCart</h1>
+
+      <button className="cart-button" onClick={onCartClick}>
+         Cart
+        {cartItemCount > 0 && (
+          <span className="cart-count">{cartItemCount}</span>
+        )}
+      </button>
+
     </header>
   );
 }
